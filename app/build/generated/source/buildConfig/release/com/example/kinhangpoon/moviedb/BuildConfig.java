4554ae4eab8a6447517abd6458ffa4f3bd00b97e
harrypoon0802 <harrypoon0802@gmail.com>
2019-01-20 16:43:17 -0500

    add apk files

---
+++ app/build/generated/source/buildConfig/release/com/example/kinhangpoon/moviedb/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.kinhangpoon.moviedb;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.kinhangpoon.moviedb";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: release
+  public static final String ApiKey = "5f74a4cfed193e5f0530e6de687a8487";
+}
